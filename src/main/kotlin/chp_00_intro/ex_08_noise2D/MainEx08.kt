522f14977fe2be51b6b93a0ee125739b245b6daf
Torbjørn Vik Lunde <torbjornlunde@users.noreply.github.com>
2020-10-24 16:11:43 +0200

    Exercise I.8

---
+++ src/main/kotlin/chp_00_intro/ex_08_noise2D/MainEx08.kt
@@ -0,0 +1,48 @@
+package chp_00_intro.ex_04_random_splatter
+
+import org.openrndr.application
+import org.openrndr.color.ColorHSLa
+import org.openrndr.color.ColorRGBa
+import org.openrndr.draw.*
+import org.openrndr.extensions.Screenshots
+import org.openrndr.extra.noise.gaussian
+import org.openrndr.extra.noise.perlin
+import org.openrndr.extra.noise.random
+import org.openrndr.math.Vector2
+import org.openrndr.math.map
+import utils.QuitOnEsc
+
+fun main() = application {
+    configure {
+        width = 768
+        height = 576
+    }
+
+    program {
+        extend(QuitOnEsc())
+        extend(Screenshots())
+
+        val seed = random(0.0, 10000.0).toInt()
+
+        val cb = colorBuffer(width, height)
+
+        // Download cb's contents so it's ready to manipulate
+        cb.shadow.download()
+
+        for (x in 0 until width) {
+            for (y in 0 until height) {
+                val hueSat = perlin(seed, x * 0.0022, y * 0.003)
+                val lum = map(0.0, 1.0, 0.25, 1.5, perlin(seed, x * 0.008, y * 0.002))
+                val color = ColorHSLa(hueSat * 0.001, hueSat * 0.25, lum, 1.0)
+                cb.shadow[x, y] = color.toRGBa()
+            }
+        }
+
+        // Upload shadow so cb get's updated
+        cb.shadow.upload()
+
+        extend {
+            drawer.image(cb)
+        }
+    }
+}
---
+++ src/main/kotlin/chp_00_intro/ex_08_noise2D/README.md
@@ -0,0 +1,9 @@
+# Intro - Exercise I.8
+
+> Play with color, noiseDetail(), and the rate at which xoff and yoff are incremented to achieve different visual effects.
+
+[Link](http://natureofcode.com/book/introduction/#intro_exercise8)
+
+![Screenshot](img1.png)
+
+![Screenshot](img2.png)
