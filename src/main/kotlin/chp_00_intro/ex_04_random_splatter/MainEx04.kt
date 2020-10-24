dfdd8860728308e5a1aa2d99faba56df1e6ccdc0
Torbjørn Vik Lunde <torbjornlunde@users.noreply.github.com>
2020-10-24 09:50:35 +0200

    Exercise 00.4

---
+++ src/main/kotlin/chp_00_intro/ex_04_random_splatter/MainEx04.kt
@@ -0,0 +1,44 @@
+package chp_00_intro.ex_04_random_splatter
+
+import org.openrndr.application
+import org.openrndr.color.ColorHSLa
+import org.openrndr.color.ColorRGBa
+import org.openrndr.draw.*
+import org.openrndr.extra.noise.gaussian
+import org.openrndr.math.Vector2
+
+fun main() = application {
+    configure {
+        width = 768
+        height = 576
+    }
+
+    program {
+        val rt = renderTarget(width, height) { colorBuffer() }
+        drawer.isolatedWithTarget(rt) {
+            drawer.clear(ColorRGBa.BLACK)
+        }
+
+        // Vector versions of gaussian values
+        val mean = drawer.bounds.dimensions / 2.0
+        val deviation = Vector2.ONE * 100.0
+
+        // Unchanging color values
+        val saturation = 0.75
+        val lightness = 0.5
+        val alpha = 0.66
+
+        extend {
+            drawer.isolatedWithTarget(rt) {
+                val position = Vector2.gaussian(mean, deviation)
+                val hue = gaussian(128.0, 50.0)
+
+                drawer.stroke = ColorRGBa.TRANSPARENT
+                drawer.fill = ColorHSLa(hue, saturation, lightness, alpha).toRGBa()
+                drawer.circle(position, 20.0)
+            }
+
+            drawer.image(rt.colorBuffer(0))
+        }
+    }
+}
