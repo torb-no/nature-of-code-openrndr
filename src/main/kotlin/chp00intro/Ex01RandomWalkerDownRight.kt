f1384ced4d83af0ab85ce43c3be7d7f3f8c03911
Torbjørn Vik Lunde <torbjornlunde@users.noreply.github.com>
2020-10-24 08:49:16 +0200

    Exercise I.1

---
+++ src/main/kotlin/chp00intro/Ex01RandomWalkerDownRight.kt
@@ -0,0 +1,53 @@
+package chp00intro
+
+import org.openrndr.application
+import org.openrndr.color.ColorRGBa
+import org.openrndr.draw.*
+import org.openrndr.extra.noise.random
+
+class Walker(val drawer: Drawer) {
+    var x = drawer.bounds.dimensions.x / 2.0
+    var y = drawer.bounds.dimensions.y / 2.0
+
+    fun update() {
+        val r = random(0.0, 1.0)
+        when {
+            r < 0.28 -> x++
+            r < 0.56 -> y++
+            r < 0.75 -> x--
+            else -> y--
+        }
+    }
+
+    fun draw() {
+        drawer.stroke = ColorRGBa.TRANSPARENT
+        drawer.rectangle(x, y, 1.0, 1.0)
+    }
+}
+
+fun main() = application {
+    configure {
+        width = 768
+        height = 576
+    }
+
+    program {
+        val rt = renderTarget(width, height) { colorBuffer() }
+        drawer.isolatedWithTarget(rt) {
+            drawer.clear(ColorRGBa.BLACK)
+        }
+
+        val walker = Walker(drawer)
+
+
+        extend {
+            walker.update()
+
+            drawer.isolatedWithTarget(rt) {
+                walker.draw()
+            }
+
+            drawer.image(rt.colorBuffer(0))
+        }
+    }
+}
