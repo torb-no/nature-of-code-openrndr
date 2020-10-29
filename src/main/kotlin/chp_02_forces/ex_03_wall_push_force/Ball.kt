1807b1e6c33e93ae6a683315f96ccf015138b458
Torbjørn Vik Lunde <torbjornlunde@users.noreply.github.com>
2020-10-29 19:03:48 +0100

    Exercise 2.3

---
+++ src/main/kotlin/chp_02_forces/ex_03_wall_push_force/Ball.kt
@@ -0,0 +1,43 @@
+package chp_02_forces.ex_03_wall_push_force
+
+import org.openrndr.Program
+import org.openrndr.color.ColorRGBa
+import org.openrndr.extra.noise.random
+import org.openrndr.math.Vector2
+
+class Ball(program: Program) {
+    val drawer = program.drawer
+    val dimensions = drawer.bounds.dimensions
+
+    var position = dimensions / 3.0
+    var velocity = Vector2.ZERO
+    var acceleration = Vector2.ZERO
+
+    var sumForces = Vector2.ZERO
+    val mass = random(1.0, 10.0)
+    val color = ColorRGBa(
+            random(0.3, 0.8),
+            random(0.3, 0.8),
+            random(0.3, 0.8),
+            0.75
+    )
+
+    fun update() {
+        acceleration = sumForces / mass
+        velocity += acceleration
+        position += velocity
+
+        sumForces = Vector2.ZERO
+    }
+
+    fun display() {
+        drawer.stroke = ColorRGBa.TRANSPARENT
+        drawer.fill = color
+        val radius = mass * 10.0
+        drawer.circle(position, radius)
+    }
+
+    fun applyForce(force: Vector2) {
+        sumForces += force
+    }
+}
---
+++ src/main/kotlin/chp_02_forces/ex_03_wall_push_force/MainEx_02_03.kt
@@ -0,0 +1,52 @@
+package chp_02_forces.ex_03_wall_push_force
+
+import org.openrndr.application
+import org.openrndr.extensions.Screenshots
+import org.openrndr.math.Vector2
+import utils.QuitOnEsc
+import kotlin.math.abs
+
+fun main() = application {
+    configure {
+        width = 768
+        height = 576
+    }
+
+    program {
+        extend(QuitOnEsc())
+        extend(Screenshots())
+
+        val middle = drawer.bounds.dimensions / 2.0
+        val edgeMultiplier = 0.005
+
+        val pushThreshold = drawer.bounds.dimensions / 2.5
+
+        val gravity = Vector2(0.0, 0.1)
+        val wind = Vector2(0.01, 0.0)
+
+        val balls = (1..10).map { Ball(this) }
+
+
+        extend {
+            balls.forEach {
+                it.applyForce(gravity)
+                it.applyForce(wind)
+
+                // Edge forces
+                val diff = middle - it.position
+                val edgeForce = Vector2(
+                    if (abs(diff.x) > pushThreshold.x) diff.x * edgeMultiplier
+                    else 0.0,
+                    if (abs(diff.y) > pushThreshold.y) diff.y * edgeMultiplier
+                    else 0.0,
+                )
+                it.applyForce(edgeForce)
+
+                it.update()
+
+            }
+
+            balls.forEach { it.display() }
+        }
+    }
+}
