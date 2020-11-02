7136ec167bed4cbf13b50dd9263590eefc626af4
Torb Lunde Miranda <torb-xyz@users.noreply.github.com>
2020-11-02 21:08:15 +0100

    Exercise 2.5

---
+++ src/main/kotlin/chp_02_forces/ex_05_liquidDrag/Liquid.kt
@@ -0,0 +1,29 @@
+package chp_02_forces.ex_05_liquidDrag
+
+import org.openrndr.Program
+import org.openrndr.color.ColorRGBa
+import org.openrndr.shape.Rectangle
+
+class Liquid(
+        program: Program,
+        val area: Rectangle,
+        val coefficient: Double,
+) {
+    val drawer = program.drawer
+
+    fun process(ball: Ball) {
+        if (area.contains(ball.position - ball.radiusVec) ||
+                area.contains(ball.position + ball.radiusVec)) {
+
+            // Apply friction
+            val dragMagnitude = ball.velocity.squaredLength * coefficient
+            val dragForce = ball.velocity.normalized * -dragMagnitude
+            ball.applyForce(dragForce)
+        }
+    }
+
+    fun display() {
+        drawer.fill = ColorRGBa(0.5, 0.5, 1.0, 0.85)
+        drawer.rectangle(area)
+    }
+}
---
+++ src/main/kotlin/chp_02_forces/ex_05_liquidDrag/README.md
@@ -0,0 +1,13 @@
+# Forces - Exercise 2.5
+
+> Take a look at our formula for drag again: 
+> drag force = coefficient * speed * speed. 
+> The faster an object moves, the greater the 
+> drag force against it. In fact, an object 
+> not moving in water experiences no drag at all. 
+> Expand the example to drop the balls from different 
+> heights. How does this affect the drag as they hit the water?
+
+[Link](http://natureofcode.com/book/chapter-2-forces/#chapter02_exercise5)
+
+![Screenshot](image.png)
